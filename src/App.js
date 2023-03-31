a143c2e75d776ced79b975cad95c83aff7346ab9
GlitzyM <nosnikhoritz@gmail.com>
2023-03-31 00:03:06 -0500

    Updated with Backend Endpoints

--- src/App.js
+++ src/App.js
@@ -52,8 +52,33 @@ class App extends Component {
       boundingBox: {},
       route: "signin",
       isSignedIn: false,
+      user: {
+        id: "",
+        name: "",
+        email: "",
+        entries: 0,
+        joined: "",
+      },
     };
   }
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
+
+  // componentDidMount() {
+  //   fetch("http://localhost:3000/")
+  //     .then((response) => response.json())
+  //     .then(console.log);
+  // }
 
   calculateFaceLocation = (data) => {
     const clarifiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -81,7 +106,22 @@ class App extends Component {
 
     fetch(`https://api.clarifai.com/v2/models/face-detection/outputs`, setUpClarifyApi(this.state.input))
       .then((response) => response.json())
-      .then((result) => this.displayFaceBox(this.calculateFaceLocation(result))) //console.log(result.outputs[0].data.regions[0].region_info.bounding_box)
+      .then((result) => {
+        if (result) {
+          fetch("http://localhost:3000/rank", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: this.state.user.id,
+            }),
+          })
+            .then((res) => res.json())
+            .then((count) => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
+            });
+        }
+        this.displayFaceBox(this.calculateFaceLocation(result)); //console.log(result.outputs[0].data.regions[0].region_info.bounding_box)
+      })
       .catch((error) => console.log("error", error));
   };
 
@@ -107,14 +147,14 @@ class App extends Component {
         <Animation />
         {this.state.route === "home" ? (
           <div>
-            <Rank />
+            <Rank name={this.state.user.name} entries={this.state.user.entries} />
             <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
             <FaceRecognition imageUrl={imageUrl} boundingBox={boundingBox} />
           </div>
         ) : route === "signin" ? (
-          <Signin onRouteChange={this.onRouteChange} />
+          <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
         ) : (
-          <Register onRouteChange={this.onRouteChange} />
+          <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
         )}
       </div>
     );
--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -1,37 +1,95 @@
-import React from "react";
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
-const Signin = ({ onRouteChange }) => {
-  return (
-    <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 center shadow-5">
-      <main className="pa4 black-80">
-        <div className="measure">
-          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-            <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="email-address">
-                Email
-              </label>
-              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
+class Signin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signInEmail: "",
+      signInPassword: "",
+      showPassword: false,
+    };
+  }
+  onEmailChange = (event) => {
+    this.setState({ signInEmail: event.target.value });
+  };
+
+  onPasswordChange = (event) => {
+    this.setState({ signInPassword: event.target.value });
+  };
+
+  onSubmitSignIn = () => {
+    fetch("http://localhost:3000/signin", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: this.state.signInEmail,
+        password: this.state.signInPassword,
+      }),
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        if (user.id) {
+          this.props.loadUser(user);
+          this.props.onRouteChange("home");
+        } else {
+          alert("wrong creds bro!");
+        }
+      });
+  };
+
+  toggleShowPassword = () => {
+    this.setState((prevState) => ({ showPassword: !prevState.showPassword }));
+  };
+
+  render() {
+    const { onRouteChange } = this.props;
+    const { showPassword, signInPassword } = this.state;
+    return (
+      <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 center shadow-5">
+        <main className="pa4 black-80">
+          <div className="measure">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                  Email
+                </label>
+                <input onChange={this.onEmailChange} className="pa2 input-reset ba bg-transparent hover-bg-blue hover-white w-100" type="email" name="email-address" id="email-address" />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">
+                  Password
+                </label>
+                <div className="relative">
+                  <input
+                    onChange={this.onPasswordChange}
+                    className="b pa2 input-reset ba bg-transparent hover-bg-blue hover-white w-100 pr5"
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    id="password"
+                    value={signInPassword}
+                  />
+                  <span className="absolute top-0 right-0 mr2 mt2">
+                    <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} onClick={this.toggleShowPassword} />
+                  </span>
+                </div>
+              </div>
+            </fieldset>
+            <div className="">
+              <input onClick={this.onSubmitSignIn} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
             </div>
-            <div className="mv3">
-              <label className="db fw6 lh-copy f6" htmlFor="password">
-                Password
-              </label>
-              <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
+            <div className="lh-copy mt3">
+              <p onClick={() => onRouteChange("register")} className="f6 link dim black db pointer">
+                Register
+              </p>
             </div>
-          </fieldset>
-          <div className="">
-            <input onClick={() => onRouteChange("home")} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
           </div>
-          <div className="lh-copy mt3">
-            <p onClick={() => onRouteChange("register")} className="f6 link dim black db pointer">
-              Register
-            </p>
-          </div>
-        </div>
-      </main>
-    </article>
-  );
-};
+        </main>
+      </article>
+    );
+  }
+}
 
 export default Signin;
